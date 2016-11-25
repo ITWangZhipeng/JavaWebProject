1d841a6c311fd4d2e00b93505e54daca11bacf9c
Administrator <910499019@qq.com>
2016-11-26 00:50:19 +0800

    第4次提交

--- src/Mysql.java
+++ src/Mysql.java
@@ -1,16 +1,32 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Mysql {
-    public static void main(String arg[]) {
+    public static Connection getConnection() {
+        Connection con = null;
         try {
-            Connection con = null; //定义一个MYSQL链接对象
-            Class.forName("com.mysql.jdbc.Driver").newInstance(); //MYSQL驱动
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/user", "root", "root"); //链接本地MYSQL
+            Class.forName("com.mysql.jdbc.Driver").newInstance();//加载驱动程序
+            //MYSQL驱动
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Mysql", "root", "123"); //链接本地MYSQL
             System.out.print("yes");
         } catch (Exception e) {
-            System.out.print("MYSQL ERROR:" + e.getMessage());
+            System.out.println(e.getStackTrace());
         }
-
+        return con;
+    }
+    public static void insert(){
+        Connection connection = getConnection();
+        try {
+            String sql = "INSERT INTO TABLENAME(COLUMN1, COLUMN2, COLUMN3)" +
+                    "VALUES('TOM', '123456', '910499019@qq.com')";
+            Statement statement = connection.createStatement();
+            int cout = statement.executeUpdate(sql);
+            System.out.println("向数据库插入了" + cout + "条信息");
+            connection.close();
+        }catch(Exception e){
+            System.out.println(e.getStackTrace());
+        }
     }
 }
